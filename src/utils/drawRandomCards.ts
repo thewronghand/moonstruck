191a72a57ef8a751aa0e294f25c79326892a6d7b
thewronghand <penfreak77@gmail.com>
2024-10-22 18:53:01 +0900

    feat: drawRandomCards 기능 작성: 무작위 카드 뽑기 + 방향 지정

--- src/utils/drawRandomCards.ts
+++ src/utils/drawRandomCards.ts
@@ -1,1 +1,33 @@
-function drawRandomCards(deck: { id: number });
+import { random, shuffle } from 'es-toolkit';
+import { tarotDeck } from '../data/tarotDeck';
+import { TarotCard } from '../Types/types';
+
+export function drawRandomCards(cardCount: number) {
+  const shuffledDeck = shuffle([...tarotDeck]);
+
+  const result = shuffledDeck
+    .slice(0, cardCount)
+    .map((card) => processCardDirection(card));
+  return result;
+}
+
+function processCardDirection(card: TarotCard) {
+  const direction: '정방향' | '역방향' =
+    random(0, 1) > 0.5 ? '정방향' : '역방향';
+
+  const result = {
+    id: card.id,
+    name: card.name,
+    arcanaType: card.arcanaType,
+    suit: card.suit,
+    number: card.number,
+    direction,
+    keywords:
+      direction === '정방향' ? card.upright.keywords : card.reversed.keywords,
+    description:
+      direction === '정방향'
+        ? card.upright.description
+        : card.reversed.description,
+  };
+  return result;
+}
