be887b920cca279489e445123ce30dd70c78f931
thewronghand <penfreak77@gmail.com>
2024-10-22 18:54:09 +0900

    feat: 무작위 카드 뽑기 기능 구현 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import { useState } from 'react';
 import Input from './Components/Input';
 import SpreadSelector from './Components/SpreadSelector';
+import { drawRandomCards } from './utils/drawRandomCards';
+import { DrawnTarotCard } from './Types/types';
 
 const INITIAL_CARD_COUNT = 1;
 
 function App() {
   const [cardCount, setCardCount] = useState(INITIAL_CARD_COUNT);
+  const [drawnCards, setDrawnCards] = useState<DrawnTarotCard[]>([]);
 
   function handleCardCountChange(newCardCount: number) {
     setCardCount(newCardCount);
   }
 
   function checkSubmittedForm(inputValue: string) {
-    const result = { inputValue, cardCount };
+    const drawnCardsResult = drawRandomCards(cardCount);
+    setDrawnCards(drawnCardsResult);
+    const result = { inputValue, cardCount, drawnCards };
     console.log(result);
   }
   return (
@@ -22,6 +27,24 @@ function App() {
         onCardCountChange={handleCardCountChange}
       />
       <Input onFormSubmit={checkSubmittedForm} />
+      <div>
+        <h2>결과</h2>
+        <div>
+          <h3>뽑힌 카드</h3>
+          <ul>
+            {drawnCards.map((card, index) => (
+              <li key={index}>
+                <h4>
+                  {card.name.en} ({card.name.ko})
+                </h4>
+                <p>
+                  {card.direction}: {card.description}
+                </p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
