2dc977f30b892b1ef6f7553aa30cce2499c590b3
thewronghand <penfreak77@gmail.com>
2024-10-28 20:05:51 +0900

    feat: Axios 인스턴스 추상화

--- src/api/callVertexApi.ts
+++ src/api/callVertexApi.ts
@@ -1,14 +1,9 @@
 import axios from 'axios';
 import { prompt } from '../data/prompt';
+import apiClient from './apiClient';
 
 export async function callVertexAPI(userInputWithCardInfo: string) {
-  const API_URL = `https://${
-    import.meta.env.VITE_API_REGION
-  }-aiplatform.googleapis.com/v1/projects/${
-    import.meta.env.VITE_GOOGLE_PROJECT_ID
-  }/locations/${import.meta.env.VITE_API_REGION}/publishers/${
-    import.meta.env.VITE_API_PUBLISHER
-  }/models/${import.meta.env.VITE_API_TARGET_MODEL}:rawPredict`;
+  const API_URL = `:rawPredict`;
 
   const requestPayload = {
     anthropic_version: 'vertex-2023-10-16',
@@ -28,12 +23,7 @@ export async function callVertexAPI(userInputWithCardInfo: string) {
   };
 
   try {
-    const response = await axios.post(API_URL, requestPayload, {
-      headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_GOOGLE_API_TOKEN}`,
-        'Content-Type': 'application/json',
-      },
-    });
+    const response = await apiClient.post(API_URL, requestPayload);
     console.log('Response: ', response.data);
     const messageContent = response.data.content[0].text;
     console.log(messageContent);
