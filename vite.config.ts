1f9fab1983b8f43a1feedee787d6e6ef94cea883
thewronghand <penfreak77@gmail.com>
2024-11-25 22:06:55 +0900

    chore: 서버리스 api 사용을 위한 프록시 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/auth': {
+          target: env.VITE_FIREBASE_FUNCTIONS_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/auth/, ''),
+        },
+        '/user': {
+          target: env.VITE_FIREBASE_FUNCTIONS_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/user/, ''),
+        },
+      },
+    },
+  };
+});
