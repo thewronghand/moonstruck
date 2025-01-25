8d68fcc92a318aff7539e89fbd3fda659be930e2
thewronghand <penfreak77@gmail.com>
2025-01-25 22:58:04 +0900

    refactor: 스프레드 스펙 변경에 따른 사용 로직 수정

--- src/Pages/QueryPage.tsx
+++ src/Pages/QueryPage.tsx
@@ -2,26 +2,35 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import SpreadSelector from '../Components/SpreadSelector';
 import Input from '../Components/Input';
+import type { SpreadType } from '../Types/spread';
+import { spreadOptions } from '../Types/spread';
 
-const INITIAL_CARD_COUNT = 1;
+const INITIAL_SPREAD_TYPE: SpreadType = 'SINGLE';
 
 export default function QueryPage() {
-  const [cardCount, setCardCount] = useState(INITIAL_CARD_COUNT);
+  const [spreadType, setSpreadType] = useState<SpreadType>(INITIAL_SPREAD_TYPE);
   const navigate = useNavigate();
 
-  function handleCardCountChange(newCardCount: number) {
-    setCardCount(newCardCount);
+  function handleSpreadTypeChange(newSpreadType: SpreadType) {
+    setSpreadType(newSpreadType);
   }
 
   function handleSubmit(userInput: string) {
-    navigate('/draw', { state: { userInput, cardCount } });
+    const cardCount = spreadOptions.find(option => option.value === spreadType)?.cardCount || 1;
+    navigate('/draw', { 
+      state: { 
+        userInput, 
+        spreadType,
+        cardCount 
+      } 
+    });
   }
 
   return (
     <div>
       <SpreadSelector
-        cardCount={cardCount}
-        onCardCountChange={handleCardCountChange}
+        spreadType={spreadType}
+        onSpreadTypeChange={handleSpreadTypeChange}
       />
       <Input onFormSubmit={handleSubmit} />
     </div>
