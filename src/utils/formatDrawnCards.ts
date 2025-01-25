137a4db00b79bb38e471ba06580e21f2f7741043
thewronghand <penfreak77@gmail.com>
2025-01-26 00:26:55 +0900

    chore: 불필요한 포매팅 함수 삭제

--- src/utils/formatDrawnCards.ts
+++
@@ -1,9 +0,0 @@
-import { DrawnTarotCard } from '../Types/tarotCard';
-
-export function formatDrawnCards(cards: DrawnTarotCard[]): string {
-  return cards
-    .map((card, idx) => {
-      return `- ${idx + 1}: ${JSON.stringify(card)}`;
-    })
-    .join('\n');
-}
